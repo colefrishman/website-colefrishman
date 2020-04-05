0526c14f607400139739f235508c66e87caeab42
Cole Frishman <awesomeeloc@gmail.com>
2020-04-04 22:20:48 -0400

    fixed text

--- src/Pages/DVD.js
+++ src/Pages/DVD.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const DVD = () => {
     return(
-    <marquee direction={"down"} behavior={"alternate"} height={"100%"} scrollamount={20}>
-        <marquee behavior={"alternate"} width={"100%"} scrollamount={20}>
-           <div style={{"font-size": "36px", "font-family": 'Segoe UI'}}><b>DVD</b></div>
+    <marquee direction={"down"} behavior={"alternate"} height={"100%"} scrollamount={25}>
+        <marquee behavior={"alternate"} width={"100%"} scrollamount={25}>
+           <div style={{"font-size": "4rem", "font-family": 'Segoe UI'}}><b>DVD</b></div>
         </marquee>
     </marquee>
     );
