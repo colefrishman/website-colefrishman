4d026f7f1e6d17ca447f4cea898236322eb1ad2a
Cole Frishman <awesomeeloc@gmail.com>
2020-04-23 19:37:43 -0400

    When's melee backbones

---
+++ src/WhensMelee/WhensMelee.js
@@ -0,0 +1,69 @@
+import React, {useState, useEffect} from 'react';
+import TournamentCard from './TournamentCard'
+
+const WhensMelee = () => {
+	const [tournamentsList, setTournamentsList] = useState([])
+	let query = `query TournamentsByVideogame {
+		tournaments(query: {
+		  perPage: 3
+		  page: 1
+		  sortBy: "startAt asc"
+		  filter: {
+			past: false,
+			videogameIds: [
+			  1
+			],
+			afterDate: ${Math.floor((new Date().getTime())/1000)},
+			published: true
+		  }
+		}) {
+		  nodes {
+			id
+			name
+			slug
+			numAttendees
+			startAt
+			endAt
+			venueName
+		  }
+		}
+	  }`;
+
+	const fetchTournaments = () => {
+		fetch('https://api.smash.gg/gql/alpha', {
+			method: 'POST',
+			headers: {
+			  'Content-Type': 'application/json',
+			  'Accept': 'application/json',
+			  'authorization': 'Bearer 45a7257fc0ac8d48d52d70218b192599'
+			},
+			body: JSON.stringify({
+			  query
+			})
+		})
+		.then(r => r.json())
+		.then(result => setTournamentsList(result.data.tournaments.nodes))
+	}
+
+	const TournamentsToList = (tournaments) => {
+
+		let JSX = tournaments.map((tournament) => {
+			return(<TournamentCard tournament={tournament} />);
+		});
+		
+		return JSX;
+	}
+
+	useEffect(()=>{
+		fetchTournaments();
+	}, [])
+
+	return(
+		<div>
+			<h1>Who's Melee?</h1>
+			{TournamentsToList(tournamentsList)}
+		</div>
+	);
+}
+
+export default WhensMelee;
