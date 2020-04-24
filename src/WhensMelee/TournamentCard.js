a64ba55d5f532ddf5aed5b1386b0fc8b50cd46cb
Cole Frishman <awesomeeloc@gmail.com>
2020-04-23 23:06:04 -0400

    When's melee styling

--- src/WhensMelee/TournamentCard.js
+++ src/WhensMelee/TournamentCard.js
@@ -1,16 +1,23 @@
 import React, {useState} from 'react';
+import Button from '@material-ui/core/Button';
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
 
 const TournamentCard = (props) => {
-
+	const tournament = props.tournament;
+	console.log(tournament)
 	return(
-		<div>
-			<h3>{props.tournament.name}</h3>
-			<p>{props.tournament.venueName}</p>
-			<p>{props.tournament.numAttendees} attendees</p>
-			<p>Starts at {(new Date(props.tournament.startAt*1000)).toString()}</p>
-			<a href={`https://smash.gg/${props.tournament.slug}`}>View on Smash.gg</a>
+		<ExpansionPanel>
+			<ExpansionPanelSummary>
+				<img src={tournament.images[0].url} alt={tournament.name} height="100px"/>
+				<h3>{tournament.name}</h3>
+			</ExpansionPanelSummary>
+			<p>{tournament.venueName}</p>
+			<p>{tournament.numAttendees} attendees</p>
+			<p>Starts at {(new Date(tournament.startAt*1000)).toString()}</p>
+			<Button href={`https://smash.gg/${tournament.slug}`} variant="contained" color="secondary">View on Smash.gg</Button>
 			<br />
-		</div>
+		</ExpansionPanel>
 	)
 }
 
